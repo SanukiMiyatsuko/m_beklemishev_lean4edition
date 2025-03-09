46496b7a6b7563537d4ecba5f8a9bff8454fd2ef
taketori <naoki123453@gmail.com>
2025-03-09 21:41:29 +0900

    parentに関する命題を作ってinnerLoopのmatchを削除

--- MBeklemishev/Basic.lean
+++ MBeklemishev/Basic.lean
@@ -47,6 +47,45 @@ theorem parent_lt_seqLength {m p_m : Nat} {seq : List Nat}
         exact (List.mem_filter.mp mem_max).left
         exact parent_lt_seqLength h0 h'
 
+theorem parent_none_up (h0 : n < m) (h1 : parent seq n = none) : parent seq m = none :=
+by
+  obtain ⟨k, hk⟩ : ∃ k, m = n + 1 + k := Nat.exists_eq_add_of_le h0
+  cases k with
+  | zero =>
+    simp at hk
+    rw [hk]
+    simp [parent]
+    simp [parentCandidates]
+    rw [h1]
+  | succ k' =>
+    let m' := n + 1 + k'
+    have m'_def : m' = n + 1 + k' := rfl
+    have n_lt_m' : n < m' :=
+      by
+        rw [m'_def]
+        apply Nat.lt_add_right k'
+        exact Nat.lt_add_one n
+    have ih : parent seq m' = none := parent_none_up n_lt_m' h1
+    have m_eq_succm' : m = m'.succ :=
+      by
+        rw [hk, m'_def]
+        rfl
+    rw [m_eq_succm']
+    simp [parent]
+    simp [parentCandidates]
+    rw [ih]
+
+theorem parent_some_down (h0 : n < m) (h1 : parent seq m = some p_m) : ∃ p_n, parent seq n = some p_n :=
+  by
+    by_cases H : ∃ p, parent seq n = some p
+    case pos => exact H
+    case neg =>
+      push_neg at H
+      have H_none : parent seq n = none := Option.eq_none_iff_forall_not_mem.mpr H
+      have h_up : parent seq m = none := parent_none_up h0 H_none
+      rw [h1] at h_up
+      contradiction
+
 def uncle (seq : List Nat) (x : Nat) (n : Nat) : Nat :=
   let filterd := (parentCandidates seq n).filter (fun i => x < i)
   filterd.min?.getD (seq.length - 1)
@@ -56,17 +95,28 @@ def fosterParent (seq : List Nat) (x : Nat) (n : Nat) : Nat :=
   | 0 => x
   | n' + 1 => fosterParent seq (uncle seq x n) n'
 
-def innerLoop (seq : List Nat) (p_m limit : Nat) : Option Nat :=
+def innerLoop (h0 : limit ≤ m) (h1 : parent seq m = some p_m) : Option Nat :=
   let results := List.ofFn (fun (i : Fin (limit - 1)) =>
-    let n : Nat := i + 1
-    match parent seq n, parent seq i with
-    | some p_n, some p_pn =>
-      if seq.extract p_m (uncle seq p_m n) < seq.extract p_n p_pn
-      then
-        some (fosterParent seq p_m n)
-      else
-        none
-    | _, _ => none
+    let n := i.val + 1
+    have h2 : ∃ p_n, parent seq n = some p_n :=
+      by
+        refine parent_some_down ?_ h1
+        have h : n = i.val + 1 := rfl
+        rw [h]
+        refine Nat.lt_of_lt_of_le ?_ h0
+        exact Nat.add_lt_of_lt_sub i.is_lt
+    let p_n := (parent seq n).get (Option.isSome_iff_exists.mpr h2)
+    have h3 : ∃ p_i, parent seq i = some p_i :=
+      by
+        refine parent_some_down ?_ h1
+        refine Nat.lt_of_lt_of_le ?_ h0
+        apply Nat.lt_of_add_right_lt
+        exact Nat.succ_lt_of_lt_pred i.is_lt
+    let p_i := (parent seq i).get (Option.isSome_iff_exists.mpr h3)
+    if seq.extract p_m (uncle seq p_m n) < seq.extract p_n p_i then
+      some (fosterParent seq p_m n)
+    else
+      none
   )
   results.filterMap id |>.head?
 
@@ -75,14 +125,14 @@ def trueParent (seq : List Nat) (level : Nat) (h0 : seq ≠ []) : Option Nat :=
   | 0 => some (seq.length - 2)
   | level' + 1 =>
     let candidates := List.ofFn (fun (i : Fin level) =>
-      let m : Nat := i + 1
+      let m := i.val + 1
       match h : parent seq m with
       | none => none
       | some p_m =>
         if seq[p_m]'(parent_lt_seqLength h0 h) < seq.getLast h0 - 1 then
           some p_m
         else
-          innerLoop seq p_m (level' ⊓ m)
+          innerLoop (Nat.min_le_right level' m) h
     )
     match candidates.find? (fun c => c.isSome) with
     | some (some candidate) => some candidate
